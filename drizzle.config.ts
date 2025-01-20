94e27e72a6c6f2628f49f0be1be36ec39c6380ea
Trilochan Bhusal <intensivestudy.research@gmail.com>
2025-01-20 08:06:37 +0545

    fea: create baseline for building schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { DATABASE_PREFIX } from "@/lib/constants";
 
 export default defineConfig({
-  schema: "./src/server/db/schema/*.ts",
+  schema: "./src/server/db/schema/**/*.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
