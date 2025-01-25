20d7a547e0e8321ab5a925b807b74ec26f8d0aad
Trilochan Bhusal <intensivestudy.research@gmail.com>
2025-01-25 17:56:28 +0545

    feat: add wards till 10

--- src/app/(main)/(dashboard)/area/create/page.tsx
+++ src/app/(main)/(dashboard)/area/create/page.tsx
@@ -46,6 +46,9 @@ const wards = [
   { value: "5", label: "5" },
   { value: "6", label: "6" },
   { value: "7", label: "7" },
+  { value: "8", label: "8" },
+  { value: "9", label: "9" },
+  { value: "10", label: "10" },
 ];
 
 const createAreaSchema = z.object({
